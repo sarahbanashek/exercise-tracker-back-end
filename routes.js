9cdefc408cd4c7a700fc70a530da890bceac039e
Sarah Banashek <sarahmbanashek@gmail.com>
2021-03-24 15:54:07 -0700

    adding queries for % change in duration & heart rate

--- routes.js
+++ routes.js
@@ -11,7 +11,12 @@ const sqlite3 = require('sqlite3').verbose();
 
 function configureRoutes(app) {
     app.get('/', (_, res) => {
+        let today = new Date();
+        const offset = today.getTimezoneOffset();
+        let sevenDaysAgo = new Date(today.getTime() - (offset * 60000) - 604800000).toISOString().split('T')[0];
+
         let data = new Object();
+
         let db = new sqlite3.Database('./app.db', err => {
             if (err) console.error(err.message);
         });
@@ -24,9 +29,22 @@ function configureRoutes(app) {
                 if (err) throw err;
                 data.durationAvg = value['round(avg(duration), 1)'];
             });
+            db.get(`SELECT round(avg(duration), 1) 
+                    FROM exercise_events
+                    WHERE date < ?`, [sevenDaysAgo], (err, value) => {
+                if (err) throw err;
+                data.untilLastWeekDurationAvg = value['round(avg(duration), 1)'];
+            });
             db.get('SELECT round(avg(heart_rate), 1) FROM exercise_events', [], (err, value) => {
                 if (err) throw err;
                 data.heartRateAvg = value['round(avg(heart_rate), 1)'];
+            });
+            db.get(`SELECT round(avg(heart_rate), 1) 
+                    FROM exercise_events
+                    WHERE date < ?`, [sevenDaysAgo], (err, value) => {
+                if (err) throw err;
+                console.log(value);
+                data.untilLastWeekHeartRateAvg = value['round(avg(heart_rate), 1)'];
             });
             db.all(`SELECT date, duration, heart_rate, description 
                     FROM exercise_events
